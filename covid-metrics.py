f710434dd030963f1c85f6a5ccf8a23f0f2c1814
JaxB04 <63479607+JaxB04@users.noreply.github.com>
2020-04-10 17:58:21 -0500

    Add files via upload

---
+++ covid-metrics.py
@@ -0,0 +1,11 @@
+# Author: Jackson Bateman jacksondbateman@gmail.com
+# Date: 04/03/2020
+
+import sys
+import csv
+
+# Main Program
+stateCode = input("What is your State Code? Example: TX: ")
+print("In " + str(stateCode) + ", x number of people contracted the disease.")
+print("In " + str(stateCode) + ", x number of people died from the disease, which is ? of the total cases.TX")
+print("In " + str(stateCode) + ", x number of people recovered from the disease, which is ? of the total cases.")
